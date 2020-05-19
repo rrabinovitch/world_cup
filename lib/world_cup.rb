0a55eca2f9ab2b715d725aadfd65d6cc05d7258e
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-18 18:28:14 -0600

    Refactor active_players_by_position method

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -7,16 +7,9 @@ class WorldCup
   end
 
   def active_players_by_position(position)
-    active_teams = @teams.find_all do |team|
-      team.eliminated? == false
-    end
-
-    active_players = active_teams.map do |team|
-      team.players
-    end.flatten
-
-    active_players.find_all do |player|
-      player.position == position
+    active_teams = @teams.reject { |team| team.eliminated? == true}
+    active_teams.flat_map do |team|
+      team.players_by_position(position)
     end
   end
 end
